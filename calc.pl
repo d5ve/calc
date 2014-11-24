14ba1ee95efcd8fa062020c957d272ba8f678953
Dave Webb <wfh@dcw.co.nz>
2014-11-25 10:37:14 +1300

    DCW - Added pi as a recognised value

--- calc.pl
+++ calc.pl
@@ -2,10 +2,12 @@
 
 use Term::ReadLine;
 
+my $pi = 3.1415926;
 my $term = Term::ReadLine->new('Simple Perl calc');
 my $prompt = "calc> ";
 my $OUT = $term->OUT || \*STDOUT;
 while ( defined ($_ = $term->readline($prompt)) ) {
+  $_ =~ s{\bpi\b}{$pi}ixms;
   my $res = eval($_);
   warn $@ if $@;
   print $OUT $res, "\n" unless $@;
